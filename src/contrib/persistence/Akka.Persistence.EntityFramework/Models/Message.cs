3585e95f8ca56bced7e62000e10a514df67a532a
Chanan Braunstein <chanan.braunstein@pearson.com>
2015-04-11 17:02:26 -0400

    Added [Index] on PersistenceId and SequenceNr

--- src/contrib/persistence/Akka.Persistence.EntityFramework/Models/Message.cs
+++ src/contrib/persistence/Akka.Persistence.EntityFramework/Models/Message.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Akka.Persistence.EntityFramework.Models
 {
@@ -6,7 +7,9 @@ namespace Akka.Persistence.EntityFramework.Models
 	{
 		[Key]
 		public long Id { get; set; }
+		[Index]
 		public string PersistentId { get; set; }
+		[Index]
 		public long SequenceNr { get; set; }
 		public bool Deleted { get; set; }
 		public byte[] Payload { get; set; }
